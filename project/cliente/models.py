7bace8af295f25ba33315300b7fb34f040b0a317
full710 <113315804+full710@users.noreply.github.com>
2023-10-02 21:04:03 -0300

    Add: model Pais

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.nombre
