0789120c00bc0719e801a6dde026943ba88faeb1
full710 <113315804+full710@users.noreply.github.com>
2023-10-04 23:32:24 -0300

    Add Form Cliente

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "cliente"
 
 urlpatterns = [
-    path("", views.index, name="index"),    
+    path("", views.index, name="index"),
+    path("crear/", views.crear, name="crear"),    
 ]
--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,9 +1,21 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from . import models
+from . import forms, models
 
 def index(request):
-    contexto = {"nombre" : "El duende"}
     
-    return render(request, "cliente/index.html", contexto)
+    clientes = models.Cliente.objects.all()    
+    return render(request, "cliente/index.html", {"clientes": clientes} )
+
+def crear(request):
+    if request.method == "POST":
+        form = forms.ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cliente:index")
+    else:
+        form = forms.ClienteForm()
+    return render(request, "cliente/crear.html", {"form":form})
+
+
